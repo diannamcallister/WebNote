3b90fd30836342e46a6ddbfc6ab482938857ae2f
diannamcallister <dd.mcallister@hotmail.ca>
2020-11-26 01:22:12 -0500

    basic implementation of css, js files

---
+++ examples.js
@@ -0,0 +1,3 @@
+var element = document.getElementById('textstuff'); 
+element.addEventListener('click', nextToHighlight);
+
--- web_note.js
+++ web_note.js
@@ -1,32 +1,28 @@
 
-var element = document.getElementById('textstuff'); 
-// var newElement = document.getElementById('test'); 
-element.addEventListener('click', nextToHighlight);
-// newElement.addEventListener('click', nextToHighlight);
-
-function test() {
-    console.log("clicked on picture!");
-}
+// var element = document.getElementById('textstuff'); 
+// element.addEventListener('click', nextToHighlight);
 
 var selected;
+const spanClassName = "note";
+const innerSpanClassName = "hoveringNote";
 
 function nextToHighlight(e) {
+
     selected = document.getSelection();
-    // clickHandler(e);
+    createHighlightOptions();
 }
 
 function clickHandler(highlightColor, note) {
     // console.log(e.clientX); -> for box
     // console.log(e.clientY);
     // console.log(e.pageY); -> for bookmark positioning
-    console.log(selected);
     let parent = selected.anchorNode.parentElement;
     
     let before;
     let after;
 
     // need to do this check to make sure that highlight starting left of word OR starting right of word both work correclty
-    if (document.getSelection().focusOffset < document.getSelection().anchorOffset) {
+    if (selected.focusOffset < selected.anchorOffset) {
         before = selected.focusOffset;
         after = selected.anchorOffset;
     } else {
@@ -34,28 +30,13 @@ function clickHandler(highlightColor, note) {
         before = selected.anchorOffset;
     }
 
-    // a part of the parent element has been highlighted
-    if (parent.childElementCount >= 1) {
-        console.log("if");
+    if (parent.tagName.toLowerCase() === 'span' && highlightColor === 'white') {
+        // need to remove highlight from element
+        removeHighlight(parent);
+    } else if (parent.childElementCount >= -1) {
+        // a part of the parent element wants to be highlighted
 
-        let highlightedText = selected.toString();
-        // check if a space is right before was is highlighted. If it is, then 
-        let spanElement = document.createElement("span");
-        const textNode = document.createTextNode(highlightedText);
-        spanElement.appendChild(textNode);
-        if (highlightColor !== "strikethrough" && note === undefined) {
-            spanElement.style.backgroundColor = highlightColor;
-        } else if (highlightColor === "strikethrough") {
-            spanElement.style.textDecoration = "line-through";
-        } else if (note !== undefined) {
-            let innerSpanElement = document.createElement("span");
-            const noteNode = document.createTextNode(highlightedText);
-            innerSpanElement.appendChild(noteNode);
-            spanElement.style.backgroundColor = highlightColor;
-            innerSpanElement.className= "tooltiptext";
-            spanElement.appendChild(innerSpanElement);
-            spanElement.className += "test";
-        }
+        let spanElement = updateVisualOfText(highlightColor, note);
         
         beforeTextNode = document.createTextNode(selected.baseNode.data.substring(0, before));
         afterTextNode = document.createTextNode(selected.baseNode.data.substring(after, selected.baseNode.data.length));
@@ -66,11 +47,9 @@ function clickHandler(highlightColor, note) {
         // need to make sure that a selection section is not in what is already highlighted.
         // if there is a highlighted section in the middle of what is selected, nothing will be highlighted
         if (selected.focusNode.parentNode.tagName.toLowerCase() !== 'span' && selected.toString() == trimmed) {
-            console.log(selected);
             // need to check if the selected element is the first child of the parent element.
             // if it is, then a specific action needs to be taken (in else)
             if (selected.anchorNode.previousSibling !== null) {
-                console.log("if if");
                 prevSibling = selected.anchorNode.previousSibling;
                 curSibling = selected.anchorNode.previousSibling.nextSibling;
 
@@ -78,95 +57,132 @@ function clickHandler(highlightColor, note) {
                 parent.insertBefore(beforeTextNode, spanElement);
                 parent.insertBefore(afterTextNode, spanElement.nextSibling);
             } else {
-                console.log("if else");
                 // we are choosing stuff from the first child of the parent
                 parent.insertBefore(afterTextNode, parent.childNodes[1]);
                 parent.replaceChild(spanElement, parent.childNodes[0]);
                 parent.insertBefore(beforeTextNode, spanElement);
             } 
         }
-    } else if (parent.tagName.toLowerCase() !== 'span'){ // the selected section is the first part of the parent that will be highlighted
-        console.log("else");
-
-        // otherwise, it does not have any siblings
-        let highlightedText = selected.toString();
-        let spanElement = document.createElement("span");
-        const textNode = document.createTextNode(highlightedText);
-        spanElement.appendChild(textNode);
-        if (highlightColor !== "strikethrough" && note === undefined) {
-            spanElement.style.backgroundColor = highlightColor;
-        } else if (highlightColor === "strikethrough") {
-            spanElement.style.textDecoration = "line-through";
-        } else if (note !== undefined) {
-            let innerSpanElement = document.createElement("span");
-            const noteNode = document.createTextNode(highlightedText);
-            innerSpanElement.appendChild(noteNode);
-            spanElement.style.backgroundColor = highlightColor;
-            innerSpanElement.className= "tooltiptext";
-            spanElement.appendChild(innerSpanElement);
-            spanElement.className += "test";
-        }
-        beforeTextNode = document.createTextNode(selected.baseNode.data.substring(0, before));
-        afterTextNode = document.createTextNode(selected.baseNode.data.substring(after, selected.baseNode.data.length));
-
-        if (selected.anchorNode.previousSibling !== null) {
-            console.log("if if2");
-            prevSibling = selected.anchorNode.previousSibling;
-            curSibling = selected.anchorNode.previousSibling.nextSibling;
-            console.log(curSibling);
-            console.log(beforeTextNode);
-            console.log(spanElement);
-
-            parent.replaceChild(spanElement, curSibling);
-            parent.insertBefore(beforeTextNode, spanElement);
-            parent.insertBefore(afterTextNode, spanElement.nextSibling);
-        } else {
-            console.log("if else2");
-            // we are choosing stuff from the first child of the parent
-            parent.insertBefore(afterTextNode, parent.childNodes[1]);
-            parent.replaceChild(spanElement, parent.childNodes[0]);
-            parent.insertBefore(beforeTextNode, spanElement);
-        } 
-
-    } else if (parent.tagName.toLowerCase() === 'span' && highlightColor === "white") {
-        // the highlighted text is no longer highlighted
-        let previousText;
-        let nextText;
-        if (selected.anchorNode.parentElement.previousElementSibling === null) {
-            previousText = selected.anchorNode.parentElement.previousSibling.data;
-        }
-        if (selected.anchorNode.parentElement.nextElementSibling === null) {
-            nextText = selected.anchorNode.parentElement.nextSibling.data;
-        }
-        if (previousText && nextText) {
-            // the text surrounding the current highlighted text is not highlighted
-            // combine all into one text node
-            const allText = previousText + parent.innerHTML + nextText;
-            const newTextNode = document.createTextNode(allText);
-            parent.parentElement.replaceChild(newTextNode, parent.previousSibling);
-            parent.parentElement.removeChild(parent.nextSibling);
-            parent.parentElement.removeChild(parent);
-        } else if (previousText) {
-            // the text before the highlighted text is not highlighted
-            const allText = previousText + parent.innerHTML;
-            const newTextNode = document.createTextNode(allText);
-            parent.parentElement.replaceChild(newTextNode, parent.previousSibling);
-            parent.parentElement.removeChild(parent);
-        } else if (nextText) {
-            // the text after the highlighted text is not highlighted
-            const allText = parent.innerHTML + nextText;
-            const newTextNode = document.createTextNode(allText);
-            parent.parentElement.removeChild(parent.nextSibling);
-            parent.parentElement.replaceChild(newTextNode, parent);
-        } else {
-            // the text before & after the higlighted text is highlighted
-            const unHighlightedText = parent.innerHTML;
-            const newTextNode = document.createTextNode(unHighlightedText);
-            parent.parentElement.replaceChild(newTextNode, parent);
-        }
-
     } else if (parent.tagName.toLowerCase() === 'span' && highlightColor === "strikethrough") {
         // add a strikethrough to something that is already highlighted
         parent.style.textDecoration = "line-through";
     }
+
+    removeHighlightOptions();
+}
+
+function updateVisualOfText(highlightColor, note) {
+    let highlightedText = selected.toString();
+    // check if a space is right before was is highlighted. If it is, then 
+    let spanElement = document.createElement("span");
+    const textNode = document.createTextNode(highlightedText);
+    spanElement.appendChild(textNode);
+    if (highlightColor !== "strikethrough" && note === undefined) {
+        // the text will get highlighted
+        spanElement.style.backgroundColor = highlightColor;
+    } else if (highlightColor === "strikethrough") {
+        // the text will be striked out
+        spanElement.style.textDecoration = "line-through";
+    } else if (note !== undefined) {
+        // a note will be added to the text
+        let innerSpanElement = document.createElement("span");
+        const noteNode = document.createTextNode(highlightedText);
+        innerSpanElement.appendChild(noteNode);
+        spanElement.style.backgroundColor = highlightColor;
+        innerSpanElement.className= "tooltiptext";
+        spanElement.appendChild(innerSpanElement);
+        spanElement.className += "test";
+    }
+    return spanElement;
+}
+
+function removeHighlight(parent) {
+    let previousText;
+    let nextText;
+    if (selected.anchorNode.parentElement.previousElementSibling === null) {
+        previousText = selected.anchorNode.parentElement.previousSibling.data;
+    }
+    if (selected.anchorNode.parentElement.nextElementSibling === null) {
+        nextText = selected.anchorNode.parentElement.nextSibling.data;
+    }
+    if (previousText && nextText) {
+        // the text surrounding the current highlighted text is not highlighted
+        // combine all into one text node
+        const allText = previousText + parent.innerHTML + nextText;
+        const newTextNode = document.createTextNode(allText);
+        parent.parentElement.replaceChild(newTextNode, parent.previousSibling);
+        parent.parentElement.removeChild(parent.nextSibling);
+        parent.parentElement.removeChild(parent);
+    } else if (previousText) {
+        // the text before the highlighted text is not highlighted
+        const allText = previousText + parent.innerHTML;
+        const newTextNode = document.createTextNode(allText);
+        parent.parentElement.replaceChild(newTextNode, parent.previousSibling);
+        parent.parentElement.removeChild(parent);
+    } else if (nextText) {
+        // the text after the highlighted text is not highlighted
+        const allText = parent.innerHTML + nextText;
+        const newTextNode = document.createTextNode(allText);
+        parent.parentElement.removeChild(parent.nextSibling);
+        parent.parentElement.replaceChild(newTextNode, parent);
+    } else {
+        // the text before & after the higlighted text is highlighted
+        const unHighlightedText = parent.innerHTML;
+        const newTextNode = document.createTextNode(unHighlightedText);
+        parent.parentElement.replaceChild(newTextNode, parent);
+    }
+}
+
+function createHighlightOptions() {
+    const highlightDiv = document.getElementById("highlight");
+
+    if (highlightDiv === null) {
+        let outerDiv = document.createElement("div");
+        let innerDiv = document.createElement("div");
+        outerDiv.id = "highlight";
+        innerDiv.id = "highlightOptions";
+
+        let greenDiv = document.createElement("img");
+        greenDiv.className = "dot";
+        greenDiv.style.backgroundColor = "green"; 
+        greenDiv.onclick = () => clickHandler("green");
+
+        let pinkDiv = document.createElement("img");
+        pinkDiv.className = "dot";
+        pinkDiv.style.backgroundColor = "pink"; 
+        pinkDiv.id = "pink";
+        pinkDiv.onclick = () => clickHandler("pink");
+
+        let orangeDiv = document.createElement("img");
+        orangeDiv.className = "dot";
+        orangeDiv.style.backgroundColor = "orange"; 
+        orangeDiv.onclick = () => clickHandler("orange");
+
+        let yellowDiv = document.createElement("img");
+        yellowDiv.className = "dot";
+        yellowDiv.style.backgroundColor = "yellow";
+        yellowDiv.onclick = () => clickHandler("yellow");
+
+        let whiteDiv = document.createElement("img");
+        whiteDiv.className = "dot";
+        whiteDiv.style.backgroundColor = "white";
+        whiteDiv.onclick = () => clickHandler("white");
+
+
+
+        innerDiv.appendChild(greenDiv);
+        innerDiv.appendChild(pinkDiv);
+        innerDiv.appendChild(orangeDiv);
+        innerDiv.appendChild(yellowDiv);
+        innerDiv.appendChild(whiteDiv);
+
+        outerDiv.appendChild(innerDiv);
+
+        document.body.insertBefore(outerDiv, document.body.firstChild);
+    }
+}
+
+function removeHighlightOptions() {
+    const outerDiv = document.getElementById("highlight");
+    document.body.removeChild(outerDiv);
 }
